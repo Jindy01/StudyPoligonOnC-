5cb11e613bf0a5bdab8d67330a7f11edf37d7498
Jindy01 <vip.fsafds@mail.ru>
2023-12-07 21:55:34 +0300

    I added new code

--- StadyPoligon/StadyPoligon/Trening.cpp
+++ StadyPoligon/StadyPoligon/Trening.cpp
@@ -1,20 +1,21 @@
 #include <iostream>;
+#include <algorithm>;
+
 
 using namespace std;
 
 int stack_of_arr_numbers(int numb) {
 
 	for (int i = 0; i < numb; ++i) {
-		
+		break;
 	}
+	return 0;
 }
 
 void read_arr(int* arr, int size) {
 
 	for (int i = 0; i < size; ++i) {
-		if (* (arr + i) < 6) {
-			continue;
-		}
+
 		cout << *(arr + i) << " " << endl;
 
 	}
@@ -22,8 +23,14 @@ void read_arr(int* arr, int size) {
 }
 
 int main() {
-
+	cout << "First arr" << endl;
 	int arr[] = { 1, 5, 6, 2, 8 };
+	read_arr(arr, sizeof(arr) / sizeof(arr[0]));
+
+	
+
+	sort(begin(arr), end(arr));
+	cout << "Second arr" << endl;
 	read_arr(arr, sizeof(arr) / sizeof(arr[0]));
 
 	return 0; 
